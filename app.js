5ae7785d27a5f911499b7e5277fa7a14fb50ea94
Rémy Lenoir <remylenoir.dev@tuta.io>
2019-05-13 19:44:00 +0200

    Moved the sendFile TO THE BOTTOM

--- app.js
+++ app.js
@@ -50,10 +50,7 @@ app.use(
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
-app.use((req, res, next) => {
-  // If no routes match, send them the React HTML.
-  res.sendFile(__dirname + '/public/index.html');
-});
+
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 hbs.registerHelper('ifUndefined', (value, options) => {
@@ -109,5 +106,9 @@ app.use('/api/ngo', ngoRoutes);
 
 const categoryRoutes = require('./routes/category');
 app.use('/api/categories', categoryRoutes);
+
+app.use((req, res) => {
+  res.sendFile('/public/index.html');
+});
 
 module.exports = app;
