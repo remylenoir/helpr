362957f41b1c67f46a920b3a5e251efd24f6048d
Rémy Lenoir <remylenoir.dev@tuta.io>
2019-05-13 19:45:57 +0200

    built app

---
+++ client/public/precache-manifest.8bb09c348e0f3e703f8aa1cd6f061092.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "928884040c35473e75cab4c31613fecd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "793e6e4555d52e2dee43",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "a76f4b703af8162a2eb7",
+    "url": "/static/css/main.dd637e4a.chunk.css"
+  },
+  {
+    "revision": "793e6e4555d52e2dee43",
+    "url": "/static/js/2.35dcd52d.chunk.js"
+  },
+  {
+    "revision": "a76f4b703af8162a2eb7",
+    "url": "/static/js/main.4aba9c09.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
