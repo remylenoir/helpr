3ef3bff8cc08cbe22db2ca02c4ac69be67e8ad79
Rémy Lenoir <remylenoir.dev@tuta.io>
2019-05-14 17:58:13 +0200

    Pop Menu feature :sparkles:

--- client/src/components/layout/AppBar/AppBar.js
+++ client/src/components/layout/AppBar/AppBar.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react';
-import PropTypes from 'prop-types';
 
-import AppIcon from './AppIcon';
+import Item from './Item';
+import PopMenu from './PopMenu';
 
 // Bootstrap components
 import Navbar from 'react-bootstrap/Navbar';
@@ -10,11 +10,13 @@ const AppMenu = () => {
   return (
     <Navbar collapseOnSelect fixed='bottom' expand='true' bg='primary'>
       <Fragment>
-        <AppIcon url={'/'} icon={'fa-home'} title={'Home'} />
-        <AppIcon url={'#'} icon={'fa-search'} title={'Events'} />
-        <AppIcon url={'#'} icon={'fa-plus'} title={'Create'} />
-        <AppIcon url={'#'} icon={'fa-map-marker-alt'} title={'Map'} />
-        <AppIcon url={'#'} icon={'fa-bookmark'} title={'Bookmarks'} />
+        <Item url={'/'} icon={'fa-home'} title={'Home'} />
+        <Item url={'#'} icon={'fa-search'} title={'Events'} />
+
+        <PopMenu />
+
+        <Item url={'#'} icon={'fa-map-marker-alt'} title={'Map'} />
+        <Item url={'#'} icon={'fa-bookmark'} title={'Bookmarks'} />
       </Fragment>
     </Navbar>
   );
