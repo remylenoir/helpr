94aecab3f0f2e658cf5df5297a2d4d3ffbfdc936
Eddie Chung <designered@gmail.com>
2019-05-15 20:08:40 +0200

    map maker and popup fixed

--- client/src/components/map/Popups.js
+++ client/src/components/map/Popups.js
@@ -3,58 +3,52 @@ import { Popup, Marker } from 'react-map-gl';
 import { getAllAlerts_ACTION } from '../../actions/alerts';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-// import Pin from './Pin';
+import Pin from './Pin';
 
 const Popups = ({ getAllAlerts_ACTION, alerts: { alerts } }) => {
   useEffect(() => {
     getAllAlerts_ACTION();
   }, []);
 
-  // const [popupState, setPopupState] = useState({
-  //   popup: false
-  // });
+  const [popupState, setPopupState] = useState(null);
 
-  const popupStyle = {
-    fontSize: '0.8em'
+  //render popups
+  const renderPopup = () => {
+    return (
+      popupState && (
+        <div id='my-map'>
+          <Popup
+            tipSize={5}
+            latitude={popupState.location.coordinates[0]}
+            longitude={popupState.location.coordinates[1]}
+            closeOnClick={false}
+            onClose={() => setPopupState(null)}
+            sortByDepth={true}
+          >
+            <div className='popup-inner'>
+              <Link to={`/alert/${popupState._id}`}>{popupState.type}</Link>
+            </div>
+          </Popup>
+        </div>
+      )
+    );
   };
-
-  const popupElements =
-    alerts &&
-    alerts.map((alert, i) => {
-      return (
-        <Popup
-          key={i}
-          latitude={alert.location.coordinates[0]}
-          longitude={alert.location.coordinates[1]}
-          closeButton={false}
-          closeOnClick={true}
-          anchor='top'
-        >
-          <div style={popupStyle}>
-            <Link to={`/alert/${alert._id}`}>{alert.type}</Link>
-          </div>
-        </Popup>
-      );
-    });
 
   return (
     <Fragment>
-      {/* Comment out for later iterations */}
-      {/* {alerts &&
+      {alerts &&
         alerts.map((alert, i) => {
           return (
             <Marker
               key={i}
               latitude={alert.location.coordinates[0]}
               longitude={alert.location.coordinates[1]}
-              onClick={() => setPopupState()}
             >
-              <Pin size={25} />
+              <Pin size={25} onClick={() => setPopupState(alert)} />
             </Marker>
-          )  
-        })} */}
-
-      {popupElements}
+          );
+        })}
+      {alerts && renderPopup()}
     </Fragment>
   );
 };
