130fa1c23a91406d97336f160a47748535a0199d
Rémy Lenoir <remylenoir.dev@tuta.io>
2019-05-17 01:36:02 +0200

    Popmenu fixed :bug:

--- client/src/components/layout/AppBar/PlusItem.js
+++ client/src/components/layout/AppBar/PlusItem.js
@@ -3,10 +3,17 @@ import OurFontAwesome from '../OurFontAwesome';
 import { Link } from 'react-router-dom';
 
 const PlusItem = ({ title, icon, url, position }) => {
+  const handleClick = () => {
+    document.querySelector('#triggerButton').checked = false;
+  };
+
   return (
     <Fragment>
       <Link to={url}>
-        <div className={`${position} d-flex justify-content-center align-items-center`}>
+        <div
+          className={`${position} d-flex justify-content-center align-items-center`}
+          onClick={handleClick}
+        >
           <div className='text-white text-center'>
             <OurFontAwesome icon={icon} />
             <h6>{title}</h6>
--- client/src/components/layout/AppBar/PopMenu.js
+++ client/src/components/layout/AppBar/PopMenu.js
@@ -7,11 +7,15 @@ const CreateIcon = () => {
     if (event.target !== popLabel) document.querySelector('#triggerButton').checked = false;
   });
 
+  const handleClick = () => {
+    document.querySelector('#trigger-close').checked = !document.querySelector('#triggerButton').checked;
+  };
+
   return (
     <Fragment>
       <div className='position-relative create-container'>
         <input id='triggerButton' className='triggerButton d-none' type='checkbox' />
-        <label htmlFor='triggerButton' id='trigger-close' />
+        <label htmlFor='triggerButton' id='trigger-close' onClick={handleClick} />
         <PlusItem title={'Alert'} icon={'fa-exclamation'} url={'/create/alert'} position={'one'} />
         <PlusItem title={'Event'} icon={'fa-calendar-alt'} url={'/create/event'} position={'two'} />
       </div>
