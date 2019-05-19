6cdde9eb8a35e420ea431d8d350edcdff55bcd39
Rémy Lenoir <remylenoir.dev@tuta.io>
2019-05-19 21:52:34 +0200

    Ready to deploy fixes :rocket:

---
+++ public/precache-manifest.02ffb00278b64eedf39c18fb592486d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "422a13132e3cf8621270c404f850a90f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a25f5479ee00cead6fa",
+    "url": "/static/css/2.ab8a97e8.chunk.css"
+  },
+  {
+    "revision": "b67ff6c0c0f8034a59da",
+    "url": "/static/css/main.09e41f2a.chunk.css"
+  },
+  {
+    "revision": "9a25f5479ee00cead6fa",
+    "url": "/static/js/2.49b0f011.chunk.js"
+  },
+  {
+    "revision": "b67ff6c0c0f8034a59da",
+    "url": "/static/js/main.5eb0b154.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
